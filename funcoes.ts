2d6dee9fca9a8d500a7fbb7029fcc0c76d06d8a2
MateusADantas <mahawprogramacao@gmail.com>
2024-10-03 08:44:33 -0300

    funcoes updated

--- funcoes.ts
+++ funcoes.ts
@@ -4,6 +4,11 @@ function calculaArea(base: number, altura: number): number {
 
 const calculaArea2 = (base: number, altura: number): number => base * altura;
 
+function somar(...numeros: number[]): void {
+  // numeros.reduce()
+  console.log(numeros);
+}
+
 function teste(): string | number {
   if (10 > 5) {
     return "dez maior que cinco";
