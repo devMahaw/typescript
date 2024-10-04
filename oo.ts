9e043c0e2bd47c5f97f3b6833ca78e7074897860
MateusADantas <mahawprogramacao@gmail.com>
2024-10-04 08:45:05 -0300

    oo updated

--- oo.ts
+++ oo.ts
@@ -1,44 +1,23 @@
 class Pessoa {
-    nome: string;
-    renda?: number; 
+  nome: string;
+  renda?: number;
 
-    constructor(nome: string, renda?: number) {
-        this.nome = nome;
-        this.renda = renda;
-    }
+  constructor(nome: string, renda?: number) {
+    this.nome = nome;
+    this.renda = renda;
+  }
 
-    dizOla(): string {
-        return `${this.nome} disse oi!`;
-    }
-}
-
-class ContaBancaria {
-    protected saldo: number = 0;
-    public numeroConta: number;
-
-    constructor(numeroDaConta: number) {
-        this.numeroConta = numeroDaConta;
-    }
-
-    static retornaNumeroDoBanco() {
-        return 125;
-    }
-
-    private getSaldo() {
-        return this.saldo;
-    }
-
-    depositar(valor: number) {
-        this.saldo += valor;
-    }
+  dizOla(): string {
+    return `${this.nome} disse oi!`;
+  }
 }
 
 class ContaBancariaPessoaFisica extends ContaBancaria {
-    depositar(valor: number): void {
-        this.saldo = valor * 2;
-    }
+  depositar(valor: number): void {
+    this.saldo = valor * 2;
+  }
 }
 
 const contaDoPedro = new ContaBancariaPessoaFisica(123456);
 
-ContaBancaria.retornaNumeroDoBanco()
+ContaBancaria.retornaNumeroDoBanco();
