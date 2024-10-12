a1bacc76464c9125a39ef89f439b61991825d155
MateusADantas <mahawprogramacao@gmail.com>
2024-10-12 08:47:15 -0300

    tipos updated

--- tipos_basicos.ts
+++ tipos_basicos.ts
@@ -14,12 +14,6 @@ const tecnologias: Array<string> = ["html", "css", "js"];
 
 const notas: ReadonlyArray<number> = [7, 9, 5, 8];
 
-const lista: [nome: string, estaEstudando: boolean, idade: number] = [
-  "gian",
-  true,
-  27,
-];
-
 let idadeDaAna: number | string = 25;
 idadeDaAna = "25 anos";
 
