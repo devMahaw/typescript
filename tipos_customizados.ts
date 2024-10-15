f5c6136947cf66a48885d4b75c6d6a20206d3327
MateusADantas <mahawprogramacao@gmail.com>
2024-10-15 08:55:16 -0300

    customizados updated

--- tipos_customizados.ts
+++ tipos_customizados.ts
@@ -4,6 +4,25 @@ type aluno = {
   idade: number;
 };
 
+const alunos: aluno[] = [
+  {
+    nome: "Carlos",
+    cursos: ["Front-end", "UX/UI"],
+    idade: 27,
+  },
+  {
+    nome: "Ana",
+    cursos: ["Front-end", "Python"],
+    idade: 23,
+  },
+];
+
+alunos.push({
+  nome: "Júlia",
+  cursos: ["Arquitetura"],
+  idade: 29,
+});
+
 const novoAluno: aluno = {
   nome: "Lucas",
   idade: 32,
